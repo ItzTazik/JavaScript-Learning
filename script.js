b83ddbd47906e275de036867e181efc3d23482d9
Tazik <dsbaltmini@gmail.com>
2024-11-23 14:10:06 +0200

    First Practice

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('Hello World!')
+const paymentPerHour = 80;
+const hoursForWork = 40;
+
+const workDays = 11 - 2;
+const workHours = workDays * 5;
+
+console.log('Can I do this work? ' + (hoursForWork <= workHours));
+console.log('Payment: ' + hoursForWork * paymentPerHour + '$');
