c9005fb01210aa8296e12d88188d28aaa0401a21
Tazik <dsbaltmini@gmail.com>
2024-11-24 15:27:32 +0200

    compound interest

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-a = "2";
+const contribution = 12000;
+const houseCost = 13500;
+const rate = 0.07;
+const result = contribution * (1 + rate / 12) ** 24;
 
-console.log(number(a) + 3);
+console.log(result);
+
+if (result > 13500){
+    console.log(`Он сможет купить дом у него останется. ${result - houseCost}$`);
+    console.log(`у него останется ${result - houseCost}$`);
+} else{
+    console.log("ему не хватит денег");
+}
