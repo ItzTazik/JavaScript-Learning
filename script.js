56016abcd83ebce01c71abde38e6e3093aef60d2
Tazik <dsbaltmini@gmail.com>
2024-11-24 14:18:45 +0200

    test

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-const paymentPerHour = 80;
-const hoursForWork = 40;
+a = "2";
 
-const workDays = 11 - 2;
-const workHours = workDays * 5;
-
-console.log('Can I do this work? ' + (hoursForWork <= workHours));
-console.log('Payment: ' + hoursForWork * paymentPerHour + '$');
+console.log(number(a) + 3);
