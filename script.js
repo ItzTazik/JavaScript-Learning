39f2a334df2bad50c1cb3ddba5a878cca1930dd0
Tazik <dsbaltmini@gmail.com>
2024-11-25 17:41:06 +0200

    Deposit Sum Counter

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-const verification = prompt(`Подтвердите, что вы не робот. Введите сумму или разность чисел 7 и 15`)
-
-switch(true){
-    case Number(verification) === 22:
-    case Number(verification) === -8:
-    case verification === "Я не робот!":
-            console.log("Успех!"); 
-            break;
-     default: console.log("Вы робот!")
+function countDepositeSum(depositInUsd, month, rate){
+return depositInUsd * (1 + rate / 12) ** month;
 }
+
+const sum = countDepositeSum(prompt("Впишите свой депозит"), prompt("Впишите срок хранения в месяцах"), prompt("Впишите ставку (дробь писать через точку)"));
+console.log(sum);
